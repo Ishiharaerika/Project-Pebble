1c6ba63fd923f3dde85a7dae84fb8fc2fddbef20
石原枝林下 <87614796+Ishiharaerika@users.noreply.github.com>
2025-04-29 08:16:00 +0900

    Add files via upload

--- Test/kernel/include/kernel.h
+++ Test/kernel/include/kernel.h
@@ -13,6 +13,9 @@
 #include <psp2kern/kernel/proc_event.h>
 #include <psp2kern/kernel/processmgr.h>
 
+#define UI_WIDTH 960 // ToDo: Needs adjust!!!
+#define UI_HEIGHT 544 // ToDo: Needs adjust!!!
+
 #define EXCEPTION_NOT_HANDLED 2
 #define MAX_CALL_STACK_DEPTH 16
 #define MAX_SLOT 16
@@ -30,7 +33,6 @@
 
 #define DEFAULT_SHOW_GUI (SCE_CTRL_LTRIGGER | SCE_CTRL_START)
 #define DEFAULT_AREA_SELECT SCE_CTRL_SELECT
-#define DEFAULT_EDIT_MODE SCE_CTRL_CROSS
 #define DEFAULT_MEMORY_MODE (SCE_CTRL_LTRIGGER | SCE_CTRL_RTRIGGER)
 #define DEFAULT_CANCEL SCE_CTRL_CROSS
 #define DEFAULT_CONFIRM SCE_CTRL_CIRCLE
@@ -122,14 +124,13 @@ typedef struct
 {
     uint32_t show_gui;
     uint32_t area_select;
-    uint32_t edit;
     uint32_t cancel;
     uint32_t confirm;
 } HotkeyConfig;
 
 typedef struct
 {
-    int edit_offset;
+    int edit_offset, cursor_column;
     EditMode edit_mode;
     MemLayout mem_layout;
     HotkeyConfig hotkeys;
@@ -139,27 +140,21 @@ typedef struct
     UIState ui_state, active_area;
     ActiveBKPTSlot breakpoints[MAX_SLOT];
     uint8_t modified_value[4], cached_mem[256];
-    bool gui_visible, has_active_bp, cursor_column;
+    bool gui_visible, has_active_bp;
     uint32_t addr, base_addr, modified_addr, edit_feature, stack[64], callstack[MAX_CALL_STACK_DEPTH], stack_size, callstack_size;
 } State;
 
-typedef struct SceKernelMemBlockInfo
-{
-    SceSize size;
-    int memoryType;
-    void *mappedBase;
-    SceUInt32 access;
-    SceSize mappedSize;
-    SceKernelMemBlockType type;
-} SceKernelMemBlockInfo;
-
 extern State guistate;
-extern bool isExclusive;
+extern SceUID evtflag;
+extern uint8_t buf_index;
 extern SceUID pebble_mtx_uid;
+extern uint32_t lowest_vaddr;
+extern uint32_t *fb_bases[2];
+extern uint32_t highest_vaddr;
+extern uint32_t *userframe_base;
 extern TargetProcess g_target_process;
 extern SceArmCpuRegisters current_registers;
 
-// gui.c
 void draw_gui(void);
 void load_hotkeys(void);
 int pebble_thread(SceSize args, void *argp);
@@ -170,6 +165,7 @@ extern int module_get_export_func(SceUID pid, const char *modname, uint32_t libn
 extern int (*ksceKernelSetPHWP)(SceUID pid, SceUInt32 a2, void *WVR, SceUInt32 WCR);
 extern int (*ksceKernelSetPHBP)(SceUID pid, SceUInt32 a2, void *BVR, SceUInt32 BCR);
 
+//bool kernel_check_event(void);
 void kernel_debugger_on_create(void);
 void kernel_debugger_init(void);
 int kernel_set_hardware_breakpoint(uint32_t address);
@@ -181,11 +177,11 @@ int kernel_get_registers(SceArmCpuRegisters *user_dst);
 int kernel_get_callstack(uint32_t *user_dst, int depth);
 int kernel_get_modulelist(SceUID *user_modids, SceSize *user_num);
 int kernel_get_moduleinfo(SceKernelModuleInfo *module_info);
-int kernel_get_memblockinfo(const void *address, uint32_t info);
+int kernel_get_memblockinfo(const void *address, uint32_t *info);
 void kernel_suspend_process(void);
 void kernel_resume_process(void);
 int kernel_single_step(void);
 int kernel_read_memory(const void *src_addr, void *user_dst, SceSize size);
-int kernel_write_memory(void *user_dst, const void *user_modification, SceSize memwrite_len);
-int kernel_write_instruction(void *user_dst, const void *user_modification, SceSize memwrite_len);
+int kernel_write_memory(uint32_t user_dst, const void *user_modification, SceSize memwrite_len);
+void kernel_get_userinfo(SceUID PID_user, SceUID pebble_mtx_uid_user, uint32_t *fb_base0_user, SceUID evtflag_user);
 int register_handler(void);
